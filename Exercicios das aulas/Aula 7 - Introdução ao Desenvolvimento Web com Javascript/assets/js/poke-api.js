40ed0315e551fce44ee30b2ed027f44c7129ac9c
luccabugatti <lbugattilive@gmail.com>
2023-10-03 17:46:37 -0300

    Melhorando as requisições e fazendo algumas em paralelo

--- Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
+++ Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
@@ -1,7 +1,12 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 //offset = 0 e limit = 10 é o default apenas
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
@@ -10,14 +15,7 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     //Promise vem como readableStream, usamos esse metodo pra transformar em Json
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
-
-//Promise.all([
-//    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-//    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-//    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-//    fetch('https://pokeapi.co/api/v2/pokemon/4')
-//]).then((results) => {
-//    console.log(results)
-//})
