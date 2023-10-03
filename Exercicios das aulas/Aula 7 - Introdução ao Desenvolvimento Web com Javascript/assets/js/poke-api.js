ed03d10a575a85c55abf9b6adf6877ce234ceeee
luccabugatti <lbugattilive@gmail.com>
2023-10-03 18:44:13 -0300

    Criando uma classe de modelo de Pokemon para padronizar

--- Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
+++ Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
@@ -1,12 +1,28 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 //offset = 0 e limit = 10 é o default apenas
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
