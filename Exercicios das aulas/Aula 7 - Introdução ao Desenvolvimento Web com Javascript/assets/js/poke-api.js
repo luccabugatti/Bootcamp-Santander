6e820a0ecae75c48f6891e578f048e79790a5931
luccabugatti <lbugattilive@gmail.com>
2023-10-12 00:21:02 -0300

    Ajuste no CSS do número do pokemon

--- Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
+++ Exercicios das aulas/Aula 7 - Introdução ao Desenvolvimento Web com Javascript/assets/js/poke-api.js
@@ -28,14 +28,10 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-//offset = 0 e limit = 10 é o default apenas
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
-    //Fetch retorna uma promise
-    //pegamos a resposta com o then caso for bem sucedido 
-    //Promise vem como readableStream, usamos esse metodo pra transformar em Json
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
