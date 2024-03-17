61ce6cc1b8cf5004936f0a314e19c99ac475ae68
luccabugatti <lbugattilive@gmail.com>
2024-03-17 01:36:44 -0300

    Criação de dados mockados

---
+++ Exercicios das aulas/DesafioBlogAngular/src/app/pages/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "FESTIVAL DE CANNES",
+        "description": "Descrição festival de Cannes" ,
+        "photoCover": "https://lwfiles.mycourse.app/clubeps-public/dc7f02cd159b3335678dde9063d13c00.jpeg"
+    },
+    {
+        "id": "2",
+        "title": "Festival de Veneza",
+        "description": "The 60th International Art Exhibition will take place from Saturday 20 April to Sunday 24 November, 2024",
+        "photoCover": "https://www.projetoparadiso.org.br/wp-content/uploads/2021/08/biennale-di-venezia-festival-de-veneza.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Festival de Berlim",
+        "description": "The 74th annual Berlin International Film Festival, usually called the Berlinale, took place between 15 and 25 February 2024",
+        "photoCover": "https://www.otroscines.com/images/fotos/berlinale-2024-poster.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Festival de São Paulo",
+        "description": "Mostra Internacional de Cinema em São Paulo | São Paulo International Film Festival. De 17 a 30 de Novembro 2024",
+        "photoCover": "https://ims.com.br/wp-content/uploads/2022/10/46-mostra-sp-2022_1223x1600.jpg"
+    }
+]
