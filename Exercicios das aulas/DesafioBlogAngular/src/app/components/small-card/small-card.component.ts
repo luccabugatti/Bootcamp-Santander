50b7606fd6541daff41a078d040542dc5f2c5501
luccabugatti <lbugattilive@gmail.com>
2024-03-16 23:35:15 -0300

    Desacoplando componentes da parte visual

--- Exercicios das aulas/DesafioBlogAngular/src/app/components/small-card/small-card.component.ts
+++ Exercicios das aulas/DesafioBlogAngular/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+  
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardDescription:string = ""
 
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 }
